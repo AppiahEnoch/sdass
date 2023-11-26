bcb9054bfa4be58e5a3a81d5bededd4cf1c10e84
AppiahEnoch <prignutt@gmail.com>
2023-11-25 21:52:09 -0800

    sdass

--- superadmin/page.php
+++ superadmin/page.php
@@ -7,7 +7,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title> Dr.YAW ACKAH MEMORIAL SCHOOL|</title>
+    <title> SDA Senior High School Bekwai</title>
     <link rel="icon" type="image/png" href="../devimage/logo.png" />
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
